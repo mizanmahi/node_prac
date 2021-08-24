93e6ef67d2def7a799c94f1d73fcab08598395be
mizanmahi <mizanmahi24@gmail.com>
2021-08-24 12:40:12 +0600

    file uploading with multer

--- app.js
+++ app.js
@@ -1,63 +1,54 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 
-// app.set('view engine', 'ejs');
-// app.set('views', './views') // optional and used for change the view folder
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    // constructing file name
+    const extName = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(extName, '')
+        .toLocaleLowerCase()
+        .split(' ')
+        .join('-') +
+      '-' +
+      Date.now();
 
-// @ this is a middle where which just logs the request information
-// ? application level middleware
-/* const logger = (req, res, next) => {
-  const { method, protocol, ip, originalUrl, hostname } = req;
-  const time = new Date(Date.now()).toLocaleString();
-  console.log({ method, originalUrl, protocol, hostname, ip, time});
-  next();
-};
-
-app.use(logger);
-
-app.get('/home', (req, res) => {
-  res.json({ name: 'Mizan' });
+    cb(null, fileName + extName);
+  },
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000')); */
-
-
-// @ this is a middle where which just logs the request information
-// ? router level middleware
-
-/* const adminRouter = express.Router();
-
- const logger = (req, res, next) => {
-  const { method, protocol, ip, originalUrl, hostname } = req;
-  const time = new Date(Date.now()).toLocaleString();
-  console.log({ method, originalUrl, protocol, hostname, ip, time});
-  next();
-};
-
-// @ every request of admin router will go through this middleware
-adminRouter.use(logger);
-
-adminRouter.get('/panel', (req, res, next) => {
-  res.send({name: 'AdminPanel'});
-})
-
-app.use('/admin', adminRouter);
-
-// app.use(logger);
-
-app.get('/home', (req, res) => {
-  res.json({ name: 'Home' });
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg'
+      ? cb(null, true)
+      : cb(new Error('This image format is not supported'));
+  },
+  limits: {
+    fileSize: 1000000, // 1MB
+  },
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));  */
+app.post('/', upload.single('avatar'), (req, res) => {
+  const { originalname, size } = req.file;
+  res.json({ originalname, size });
+});
 
-const adminRouter = require('./adminRouter');
+app.use((err, req, res, next) => {
+  if (!err) {
+    res.status(200).send('Success');
+  } else {
+    res.send(err.message);
+  }
+});
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
-
-app.use('/admin', adminRouter);
-
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => console.log(`Server is running on 3000`));
