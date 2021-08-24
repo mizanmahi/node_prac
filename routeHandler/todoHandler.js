1a6ad809ac2226c2e5e0827c1ca138de6f316314
mizanmahi <mizanmahi24@gmail.com>
2021-08-24 21:05:34 +0600

    done with the basic crud with mongo

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -3,16 +3,29 @@ const mongoose = require('mongoose');
 
 const todoSchema = require('../schemas/todoSchema');
 
-// crating a Todo class by calling mongoose.model class
+// crating a Todo model (class) by calling mongoose.model class
 const Todo = new mongoose.model('todo', todoSchema);
 
 const router = express.Router();
 
+// getting all todo
 router.get('/', async (req, res) => {
-  res.send('TODO GET');
+  await Todo.find({}, (err, arr) => {
+    if (!err) {
+      res.status(200).json({ message: arr });
+    }
+  });
 });
 
-router.get('/:id', async (req, res) => {});
+router.get('/:id', async (req, res) => {
+  await Todo.find({ _id: req.params.id }, (err, arr) => {
+    if (!err) {
+      res.status(200).json({ todo: arr });
+    } else {
+      res.status(500).json({ error: err.message });
+    }
+  });
+});
 
 // saving single todo
 router.post('/', async (req, res) => {
@@ -38,16 +51,28 @@ router.post('/all', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  Todo.updateOne(
+  const result = await Todo.findOneAndUpdate(
     { _id: req.params.id },
-    { $set: {description: req.body.value} },
+    { $set: { description: req.body.value } },
+    {
+      useFindAndModify: false,
+    },
     (err) =>
       err
         ? res.status(500).json({ error: err.message })
         : res.status(200).json({ message: `field is updated successfully!` })
   );
+  // console.log(result);
 });
 
-router.delete('/:id', async (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (!err) {
+      res.status(200).json({ message: 'deleted successfully!' });
+    } else {
+      res.status(500).json({ error: err.message });
+    }
+  });
+});
 
 module.exports = router;
